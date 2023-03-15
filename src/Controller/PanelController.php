f07fac61ca365fd2673ba192dac2c0ec741739fc
Grzesiek <grzesiekb@onet.pl>
2023-03-15 17:32:47 +0100

    Implemented basic template for panel

--- src/Controller/PanelController.php
+++ src/Controller/PanelController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MainController extends AbstractController
+class PanelController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/panel', name: 'app_panel')]
     public function index(): Response
     {
-        return $this->render('/index.html.twig', [
-            'controller_name' => 'MainController',
+        return $this->render('panel/index.html.twig', [
+            'controller_name' => 'PanelController',
         ]);
     }
 }
