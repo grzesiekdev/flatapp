f882f82a1bad963cb7de2b0ea4a60944a3481c10
Grzesiek <grzesiekb@onet.pl>
2023-03-28 23:37:29 +0200

    Reworked migrations

--- migrations/Version20230328172242.php
+++ migrations/Version20230328172242.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230326144828 extends AbstractMigration
+final class Version20230328172242 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -23,7 +23,7 @@ final class Version20230326144828 extends AbstractMigration
         $this->addSql('CREATE TABLE flat (id INT AUTO_INCREMENT NOT NULL, landlord_id INT NOT NULL, area INT NOT NULL, number_of_rooms INT NOT NULL, rent INT NOT NULL, fees LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:json)\', deposit INT DEFAULT NULL, pictures LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', pictures_for_tenant LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:json)\', description VARCHAR(1000) DEFAULT NULL, address VARCHAR(255) NOT NULL, rent_agreement VARCHAR(255) DEFAULT NULL, furnishing LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', INDEX IDX_554AAA44D48E7AED (landlord_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE landlord (id INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE tenant (id INT NOT NULL, flat_id INT NOT NULL, INDEX IDX_4E59C462C5FCE984 (flat_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, date_of_birth DATE NOT NULL, address VARCHAR(255) DEFAULT NULL, image VARCHAR(255) DEFAULT NULL, phone VARCHAR(12) DEFAULT NULL, discriminator VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, date_of_birth DATE NOT NULL, address VARCHAR(255) DEFAULT NULL, image VARCHAR(255) DEFAULT NULL, phone VARCHAR(12) DEFAULT NULL, is_verified TINYINT(1) NOT NULL, discriminator VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE flat ADD CONSTRAINT FK_554AAA44D48E7AED FOREIGN KEY (landlord_id) REFERENCES landlord (id)');
         $this->addSql('ALTER TABLE landlord ADD CONSTRAINT FK_F446E8F8BF396750 FOREIGN KEY (id) REFERENCES user (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE tenant ADD CONSTRAINT FK_4E59C462C5FCE984 FOREIGN KEY (flat_id) REFERENCES flat (id)');
