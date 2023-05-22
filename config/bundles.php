15ec133b19880ce87cc83ff4072c3f800be3f1f6
Grzesiek <grzesiekb@onet.pl>
2023-05-22 18:25:12 +0200

    fix: added new dependency - clearing DB in phpunit

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Craue\FormFlowBundle\CraueFormFlowBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
 ];
