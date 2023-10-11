ec88a547f12cfb4356aba22fd24d6401029dd59a
grzesiekdev <grzesiekb@onet.pl>
2023-10-11 16:47:20 +0200

    feat: added running chat-server.php to docker-compose, changed chat port to 8282

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -178,7 +178,7 @@ function handle_chat() {
                 });
 
         });
-        const conn = new WebSocket(`ws://localhost:8080?receiverId=${receiverId}&senderId=${senderId}`);
+        const conn = new WebSocket(`ws://localhost:8282?receiverId=${receiverId}&senderId=${senderId}`);
         conn.onopen = function(e) {
             console.log("Connection established!");
         };
--- bin/chat-server.php
+++ bin/chat-server.php
@@ -13,7 +13,7 @@ $server = IoServer::factory(
             new Chat()
         )
     ),
-    8080
+    8282
 );
 
 
