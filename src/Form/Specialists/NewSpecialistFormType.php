003d8011b5671141be3c6c914594dfb6a3c04711
grzesiekdev <grzesiekb@onet.pl>
2023-10-14 13:05:03 +0200

    feat: added constraints for specialists hub

--- src/Form/Specialists/NewSpecialistFormType.php
+++ src/Form/Specialists/NewSpecialistFormType.php
@@ -6,6 +6,8 @@ use App\Entity\Flat;
 use App\Entity\Specialist;
 use App\Entity\User\Type\Landlord;
 use App\Entity\User\User;
+use App\Form\DataTransformer\EmailTransformer;
+use App\Form\DataTransformer\PhoneNumberTransformer;
 use App\Repository\FlatRepository;
 use App\Repository\LandlordRepository;
 use Symfony\Component\Form\AbstractType;
@@ -13,24 +15,30 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class NewSpecialistFormType extends AbstractType
 {
     private LandlordRepository $landlordRepository;
     private string $userEmail;
     private Landlord $user;
+    private ValidatorInterface $validator;
+    private SessionInterface $session;
 
-    public function __construct(LandlordRepository $landlordRepository, Security $security)
+    public function __construct(LandlordRepository $landlordRepository, Security $security, ValidatorInterface $validator)
     {
         $this->landlordRepository = $landlordRepository;
         $this->userEmail = $security->getUser()->getUserIdentifier();
+        $this->validator = $validator;
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $session = $options['session'];
+        $this->session = $options['session'];
         $specialistFlats = $options['specialist_flats'];
 
         $this->user = $this->landlordRepository->findOneBy(['email' => $this->userEmail]);
@@ -41,11 +49,23 @@ class NewSpecialistFormType extends AbstractType
                 'attr' => ['class' => '
                         form-control',
                 ],
+                'constraints' => [
+                    new Length([
+                        'max' => 50,
+                        'maxMessage' => 'Name too long',
+                    ]),
+                ],
                 'error_bubbling' => true,
             ])
             ->add('profession', TextType::class, [
                 'attr' => ['class' => '
                         form-control',
+                ],
+                'constraints' => [
+                    new Length([
+                        'max' => 50,
+                        'maxMessage' => 'Profession too long',
+                    ]),
                 ],
                 'error_bubbling' => true,
             ])
@@ -53,32 +73,67 @@ class NewSpecialistFormType extends AbstractType
                 'attr' => ['class' => '
                         form-control',
                 ],
-                'required' => false
+                'constraints' => [
+                    new Length([
+                        'max' => 70,
+                        'maxMessage' => 'Email too long',
+                    ]),
+                ],
+                'required' => false,
+                'error_bubbling' => true,
             ])
             ->add('phone', TextType::class, [
                 'attr' => ['class' => '
                         form-control',
                 ],
-                'required' => false
+                'constraints' => [
+                    new Length([
+                        'max' => 15,
+                        'maxMessage' => 'Phone too long',
+                    ]),
+                ],
+                'required' => false,
+                'error_bubbling' => true,
             ])
             ->add('address', TextType::class, [
                 'attr' => ['class' => '
                         form-control',
                 ],
-                'required' => false
+                'constraints' => [
+                    new Length([
+                        'max' => 255,
+                        'maxMessage' => 'Address too long',
+                    ]),
+                ],
+                'required' => false,
+                'error_bubbling' => true,
             ])
             ->add('gmb', TextType::class, [
                 'attr' => ['class' => '
                         form-control',
                 ],
+                'constraints' => [
+                    new Length([
+                        'max' => 500,
+                        'maxMessage' => 'GMB address too long',
+                    ]),
+                ],
                 'required' => false,
-                'label' => 'Google My Business'
+                'label' => 'Google My Business',
+                'error_bubbling' => true,
             ])
             ->add('note', TextareaType::class, [
                 'attr' => ['class' => '
                         form-control',
                 ],
-                'required' => false
+                'constraints' => [
+                    new Length([
+                        'max' => 1000,
+                        'maxMessage' => 'Note too long',
+                    ]),
+                ],
+                'required' => false,
+                'error_bubbling' => true,
             ])
             ->add('flats', ChoiceType::class, [
                 'attr' => ['class' => '
@@ -93,6 +148,10 @@ class NewSpecialistFormType extends AbstractType
                 'expanded' => true,
                 'data' => $specialistFlats
             ]);
+        $builder->get('phone')
+            ->addModelTransformer(new PhoneNumberTransformer($this->validator, $this->session));
+        $builder->get('email')
+            ->addModelTransformer(new EmailTransformer($this->validator, $this->session));
     }
 
     public function configureOptions(OptionsResolver $resolver): void
